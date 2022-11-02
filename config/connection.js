a542b70d4ad336a6771bef13a8cb3f2bc340185f
Peyton C <peyton.castill0@icloud.com>
2022-11-02 14:22:03 -0400

    esablishing  connection

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,12 @@
 // here is where i will need to write a my connection for the server
+const { connect, connection } = require('mongoose');
+
+const connectionString =
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/studentsDB';
+
+connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+module.exports = connection;
