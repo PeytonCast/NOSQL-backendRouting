cd9c9c657b09f2e8d72f273cf65f1ee46264f1cc
Peyton C <peyton.castill0@icloud.com>
2022-11-03 15:27:52 -0400

    more server.js work

--- server.js
+++ server.js
@@ -5,15 +5,15 @@ const routes = require('./routes');
 
 const cwd = process.cwd();
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes);
+// app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => {
-    console.log(`API server for ${activity} running on port ${PORT}!`);
+    console.log(`API running on port ${PORT}!`);
   });
 });
