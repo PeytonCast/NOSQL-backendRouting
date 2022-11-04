b46249e7fd152c4b0b427c845ec44b5741a6327f
Peyton C <peyton.castill0@icloud.com>
2022-11-04 15:32:31 -0400

    adding thoughts schema

--- models/thoughts.js
+++ models/thoughts.js
@@ -1,18 +1,28 @@
 // Thought:
+const { Schema, model } = require('mongoose');
+const reactionsSchema = require('./reaction');
 
-// thoughtText
-// String
-// Required
-// Must be between 1 and 280 characters
+const thoughtsSchema = new Schema({
+    thoughtText: {
+     type: String, 
+     required: true,
+     min_length:1,
+     max_length:280
+    },
+    createdAt: {
+        type: Date, 
+        default: Date.now
+        // Use a getter method to format the timestamp on query
+    },
+    // the user who posted will be filled in here
+    username: {
+        type: String, 
+        required: true,
+    },
+    reactions: [reactionsSchema]
 
-// createdAt
-// Date
-// Set default value to the current timestamp
-// Use a getter method to format the timestamp on query
+})
 
-// username (The user that created this thought)
-// String
-// Required
+const Thoughts = model('thoughts', thoughtsSchema)
 
-// reactions (These are like replies)
-// Array of nested documents created with the reactionSchema
+module.exports = Thoughts
